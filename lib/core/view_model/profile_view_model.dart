3cf83c31e62ce3e9096c138e1a0f2884738db816
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-11-25 02:58:04 +0200

    Account Screen  UI applied

--- lib/core/view_model/profile_view_model.dart
+++ lib/core/view_model/profile_view_model.dart
@@ -2,23 +2,28 @@
 import 'package:ecommerce/core/helper/local_storage_data.dart';
 import 'package:ecommerce/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfileViewModel extends GetxController{
-  @override
-  void onInit() {
-    getCurrentUser();
-  }
+  ValueNotifier<bool> get loading => _loading ;
+  ValueNotifier <bool> _loading = ValueNotifier(false);
  final LocalStorageData localStorageData = Get.find();
   UserModel get userModel => _userModel;
   UserModel _userModel;
 
+ @override
+ void onInit() {
+   getCurrentUser();
+ }
 
   void getCurrentUser() async {
+   _loading.value = true;
     await localStorageData.getUser.then((value) {
       _userModel = value;
     });
+    _loading.value = false;
     update();
   }
 
--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce/core/view_model/profile_view_model.dart';
 import 'package:ecommerce/view/auth/login_screen.dart';
+import 'package:ecommerce/view/widgets/custom_text.dart';
+import 'package:ecommerce/view/widgets/custome_sizedbox.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class ProfileView extends StatelessWidget {
@@ -8,7 +10,8 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder <ProfileViewModel>(
       init : ProfileViewModel(),
-    builder:(controller) =>Scaffold(
+    builder:(controller) => controller.loading.value ? Center(child: CircularProgressIndicator())
+        :Scaffold(
       body: Container(
         padding:EdgeInsets.only(
           top: 50,
@@ -19,6 +22,7 @@ class ProfileView extends StatelessWidget {
               children: [
                 Container(
                  child: Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                     Container(
                       width: 100,
@@ -39,9 +43,9 @@ class ProfileView extends StatelessWidget {
                         // )
                         image: DecorationImage(
                           image: controller.userModel == null
-                              ? const AssetImage('/assets/images/avatar.png')
+                              ? const AssetImage('assets/images/avatar.png')
                               :controller.userModel.pic == 'default'
-                                ? const AssetImage('/assets/images/avatar.png')
+                                ? const AssetImage('assets/images/avatar.png')
                                 :NetworkImage(
                                     controller.userModel.pic,
                                      ),
@@ -49,10 +53,149 @@ class ProfileView extends StatelessWidget {
                         ),
                       ),
 
-                    )
+                    ),
+                      // FlatButton(onPressed: (){
+                      //   controller.Signout();
+                      //   Get.offAll(LoginScreen());
+                      // },
+                      //     child: Text('Sign out'),
+                      //
+                      // ),
+                      Column(
+                        children: [
+                          CustomText(
+                            text: controller.userModel.name,
+                            color: Colors.black,
+                            fontSize: 32,
+
+                          ),
+                          CustomText(
+                            text: controller.userModel.email,
+                            color: Colors.black,
+                            fontSize: 13,
+
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
+                CustomSizedBox(
+                  height: 100,
+                ),
+                Container(
+                  child: FlatButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title:CustomText(
+                        text: 'Shipping Address',
+                      ) ,
+                      leading:Image.asset('assets/images/Group 185.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color:Colors.black,
+                      ),
+                    ),
+                  ),
+
+                ),
+                CustomSizedBox(
+                  height: 10,
+
+                ),
+                Container(
+                  child: FlatButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title:CustomText(
+                        text: 'Order History',
+                      ) ,
+                      leading:Image.asset('assets/images/Group 194.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color:Colors.black,
+                      ),
+                    ),
+                  ),
+
+                ),
+                CustomSizedBox(
+                  height: 10,
+
+                ),
+                Container(
+                  child: FlatButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title:CustomText(
+                        text: 'Cards',
+                      ) ,
+                      leading:Image.asset('assets/images/Group 197.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color:Colors.black,
+                      ),
+                    ),
+                  ),
+
+                ),
+                CustomSizedBox(
+                  height: 10,
+
+                ),
+                Container(
+                  child: FlatButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title:CustomText(
+                        text: 'Notification',
+                      ) ,
+                      leading:Image.asset('assets/images/Group 198.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color:Colors.black,
+                      ),
+                    ),
+                  ),
+
+                ),
+                CustomSizedBox(
+                  height: 10,
+
+                ),
+                Container(
+                  child: FlatButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title:CustomText(
+                        text: 'Log out ',
+                      ) ,
+                      leading:Image.asset('assets/images/Group 200.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color:Colors.black,
+                      ),
+                    ),
+                  ),
+
+                ),
+
+
               ],
             ),
           ),
