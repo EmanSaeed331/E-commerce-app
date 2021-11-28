5635a5d9125af7d27d0d7eca41ad13474337e8b5
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-11-28 23:56:54 +0200

    summery modified

--- lib/view/summary_view.dart
+++ lib/view/summary_view.dart
@@ -1,20 +1,29 @@
-
-
+import 'package:ecommerce/core/view_model/cart_view_model.dart';
+import 'package:ecommerce/view/widgets/custome_sizedbox.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 class Summary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: Text(
-              "Summary",
-              style: TextStyle(fontSize: 30),
+      child: GetBuilder<CartViewModel>(
+        builder: (controller)=> Column(
+          children: [
+            CustomSizedBox(
+              height: 40,
             ),
-          ),
-        ],
+            Container(
+              height: 350,
+              color: Colors.red,
+
+              child: ListView.builder(itemBuilder: (context,index)=>
+                  Container(
+              ),
+                itemCount:controller.cartProductModel.length , ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
