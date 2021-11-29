4779efa69de53a73f6625cdf142e398e0ac2f344
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-11-29 05:29:07 +0200

    categories data added

---
+++ lib/view/men_products_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MenCategoryProducts extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Men Products'),
+    );
+  }
+}
